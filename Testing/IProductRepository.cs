a851a655d465666d5a607e5eeb5eb7f534a736bd
kyle jenne <strongagent1@gmail.com>
2022-11-23 12:56:13 -0500

    ASP exercise 3 for KJ

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -1,6 +1,7 @@
 ﻿using NuGet.Protocol.Plugins;
 using System.Collections.Generic;
 using System.Drawing;
+using Testing.Models;
 
 namespace Testing
 {
@@ -9,6 +10,6 @@ namespace Testing
         public IEnumerable<Product> GetAllProducts();
 
         public Product GetProduct(int id);
-
+        public void UpdateProduct(Product product);
     }
 }
